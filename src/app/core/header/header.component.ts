0bef3f088e757124f3d924ce293ff8bda234c8fb
Alexis Leroy <alexis-60@hotmail.fr>
2020-06-14 20:21:08 +0200

    logout button

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { AuthService } from '../services/auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,12 @@ import { AuthService } from '../services/auth.service'
 export class HeaderComponent {
   showAdminNav = false
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.showAdminNav = this.authService.isAdmin
+  }
+
+  logout(): void {
+    this.authService.logout()
+    this.router.navigate(['/login'])
   }
 }
