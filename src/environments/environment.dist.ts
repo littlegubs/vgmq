54b23eb0a5aae949268267169b86719c5694d36a
Florian Robert <florian.robert@ekino.com>
2020-07-23 13:57:40 +0200

    clean test

--- src/environments/environment.dist.ts
+++ src/environments/environment.dist.ts
@@ -2,7 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+export const environment: {
+  production: boolean
+  apiEndpoint: string
+  cookieDomain: string | null
+} = {
   production: false,
   apiEndpoint: 'http://localhost',
   cookieDomain: null,
