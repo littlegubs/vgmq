5495838d86ed4f83952c517b3061841ae41b44b0
lbourgeois <lucasbourgeois60@hotmail.fr>
2022-06-19 22:52:17 +0200

    add confirm delete dialog on music delete

---
+++ src/app/modules/admin/modules/games/components/game-show/components/confirm-music-delete-dialog/confirm-music-delete-dialog.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-confirm-music-delete-dialog',
+  templateUrl: './confirm-music-delete-dialog.component.html',
+})
+export class ConfirmMusicDeleteDialogComponent {}
