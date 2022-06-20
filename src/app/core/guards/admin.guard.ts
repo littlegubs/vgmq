73185cc9f60e8e3d536b084f60cfbfdb747f17e4
lbourgeois <lucasbourgeois60@hotmail.fr>
2022-06-20 22:49:04 +0200

    keep guarded url in session storage if user need to login then redirect him to the guarded url

--- src/app/core/guards/admin.guard.ts
+++ src/app/core/guards/admin.guard.ts
@@ -9,6 +9,20 @@ export class AdminGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
-    return this.authService.isAdmin ?? this.router.createUrlTree([''])
+    if (this.authService.isLoggedIn && this.authService.isAdmin) {
+      return true
+    } else if (this.authService.isLoggedIn && !this.authService.isAdmin) {
+      return this.router.createUrlTree([''])
+    } else if (!this.authService.isLoggedIn) {
+      if (state.url?.length) {
+        sessionStorage.setItem('pastedUrl', state.url.split('?')[0])
+      }
+      if (Object.keys(next.queryParams).length) {
+        sessionStorage.setItem('pastedUrlQueryParams', JSON.stringify(next.queryParams))
+      }
+      return this.router.createUrlTree(['login'])
+    } else {
+      return this.router.createUrlTree([''])
+    }
   }
 }
--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -9,6 +9,17 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
-    return this.authService.isLoggedIn || this.router.createUrlTree(['login'])
+    const isLoggedIn = this.authService.isLoggedIn
+    if (isLoggedIn) {
+      return true
+    } else {
+      if (state.url?.length) {
+        sessionStorage.setItem('pastedUrl', state.url.split('?')[0])
+      }
+      if (Object.keys(next.queryParams).length) {
+        sessionStorage.setItem('pastedUrlQueryParams', JSON.stringify(next.queryParams))
+      }
+      return this.router.createUrlTree(['login'])
+    }
   }
 }
