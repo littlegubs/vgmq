75ac0d5ebf97d2710ddd7e168b3fa5360ef4f421
lbourgeois <lucasbourgeois60@hotmail.fr>
2022-06-25 19:41:16 +0200

    wip countdown

--- src/app/modules/lobby/components/countdown/countdown.component.ts
+++ src/app/modules/lobby/components/countdown/countdown.component.ts
@@ -1,30 +1,55 @@
-import { Component, OnInit } from '@angular/core'
-import { Lobby } from '../../../../shared/models/lobby'
-import { Observable } from 'rxjs'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { LobbyStatuses } from '../../../../shared/models/lobby'
+import { LobbyStore } from '../../../../core/store/lobby.store'
+import { Subscription } from 'rxjs'
+import {AudioContext} from "angular-audio-context";
 
 @Component({
   selector: 'app-lobby-countdown',
   templateUrl: './countdown.component.html',
 })
-export class CountdownComponent implements OnInit {
-  lobby: Lobby
-  counter: Observable<number>
-  guessTime: number
+export class CountdownComponent implements OnInit, OnDestroy {
+  countdown: number
+  countdownInterval: number
+  audioContextInterval: number
+  subscriptions: Array<Subscription>
 
-  constructor() {}
+  constructor(private lobbyStore: LobbyStore, private audioContext: AudioContext
+) {}
 
   ngOnInit(): void {
-    // this.store.select('lobby').subscribe((res) => {
-    //   if (this.lobby !== undefined) {
-    //     if (this.lobby.status !== res.lobby.status) {
-    //       this.guessTime = res.lobby.guessTime
-    //       this.counter = timer(0, 1000).pipe(
-    //         take(this.guessTime),
-    //         map(() => --this.guessTime)
-    //       )
-    //     }
-    //   }
-    //   this.lobby = res.lobby
-    // })
+    this.subscriptions = [
+      this.lobbyStore.lobby.subscribe((lobby) => {
+        console.log(this.audioContext.state)
+
+        if (this.countdownInterval) {
+          clearInterval(this.countdownInterval)
+        }
+        if (lobby?.status === LobbyStatuses.PlayingMusic) {
+          this.countdown = lobby.guessTime
+          this.startCountdown()
+        } else if (lobby?.status === LobbyStatuses.AnswerReveal) {
+          this.countdown = 10
+          this.startCountdown()
+        }
+      }),
+    ]
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.countdownInterval)
+    this.subscriptions.forEach((sb) => sb.unsubscribe())
+  }
+
+  startCountdown(): void {
+    this.countdownInterval = setInterval(() => {
+      if (this.countdown > 0) {
+        console.log(this.audioContext.state)
+        this.countdown--
+      } else {
+        this.countdown = undefined
+        clearInterval(this.countdownInterval)
+      }
+    }, 1000)
   }
 }
