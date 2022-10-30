76047a3ba08fc0d400f8248897d86a110fada2ac
Alexis Leroy <alexis-60@hotmail.fr>
2022-10-30 17:08:46 +0100

    SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core'
+import { Meta, Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private meta: Meta, private titleService: Title) {
+    this.titleService.setTitle('VGMQ - Video Game Music Quiz')
+    this.meta.addTag({
+      name: 'description',
+      content: 'Play with your friends! Listen carefully and guess the game!',
+    })
+  }
+}
