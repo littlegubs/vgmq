cdf9d3264eb798a24de4d9d107249f7cf96e125a
Alexis Leroy <alexis-60@hotmail.fr>
2023-11-02 18:29:44 +0100

    new homepages

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -4,7 +4,6 @@ import { User } from 'src/app/shared/models/user'
 import { Lobby } from 'src/app/shared/models/lobby'
 import { LobbyHttpService } from '../../core/http/lobby.http.service'
 import { MatDialog } from '@angular/material/dialog'
-import { PasswordDialogComponent } from '../lobby/components/password-dialog/password-dialog.component'
 import { Subscription } from 'rxjs'
 import { LobbyListSocket } from '../../core/socket/lobby-list.socket'
 
@@ -13,12 +12,8 @@ import { LobbyListSocket } from '../../core/socket/lobby-list.socket'
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  public lobby: Lobby
-  showFilter = false
-  isPrivate = false
-  isPlaying = false
-  isFull = false
-  lobbies: Lobby[]
+  publicLobbies: Lobby[]
+  customLobbies: Lobby[]
   user: User
   subscriptions: Subscription[] = []
 
@@ -36,32 +31,15 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.lobbyHttpService.list().subscribe((res) => {
-      this.lobbies = res
+      this.publicLobbies = res.filter((lobby) => !lobby.custom)
+      this.customLobbies = res.filter((lobby) => lobby.custom)
     })
     this.socket.connect()
     this.subscriptions = [
       this.socket.fromEvent('lobbyList').subscribe((res: Lobby[]) => {
-        this.lobbies = res
+        this.publicLobbies = res.filter((lobby) => !lobby.custom)
+        this.customLobbies = res.filter((lobby) => lobby.custom)
       }),
     ]
-  }
-
-  joinLobby(lobby: Lobby): void {
-    if (lobby.hasPassword) {
-      const passwordDialog = this.dialog.open(PasswordDialogComponent, {
-        data: lobby.code,
-      })
-      passwordDialog.afterClosed().subscribe((res: Lobby | undefined) => {
-        if (res) {
-          void this.router.navigate([`/lobby/${res.code}`])
-        }
-      })
-    } else {
-      void this.router.navigate([`/lobby/${lobby.code}`])
-    }
-  }
-
-  showFilters(): void {
-    this.showFilter = !this.showFilter
   }
 }
