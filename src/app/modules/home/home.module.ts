6a4aaf88ebed7da87a8e0638da02586657dd4827
Alexis Leroy <alexis-60@hotmail.fr>
2023-11-19 20:08:20 +0100

    europe warning message

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './home.component'
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from '../../shared/shared.module'
 import { HomeLobbyComponent } from './components/home-lobby/home-lobby.component'
 
 const routes: Routes = [{ path: '', component: HomeComponent }]
