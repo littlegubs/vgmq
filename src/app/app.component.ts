8a0414470807e536e14a91a6debe8133c75ad354
Alexis Leroy <alexis-60@hotmail.fr>
2024-04-07 12:43:37 +0200

    move meta description in index.html for a better SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,7 @@
 import { Component } from '@angular/core'
-import { Meta, Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  constructor(private meta: Meta, private titleService: Title) {
-    this.titleService.setTitle('Video Game Music Quiz')
-    this.meta.addTag({
-      name: 'description',
-      content: 'Play with your friends! Listen carefully and guess the game!',
-    })
-  }
-}
+export class AppComponent {}
