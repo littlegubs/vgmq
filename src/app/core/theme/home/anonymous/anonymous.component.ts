fe7f6955b56228a20ed4cc20f0d3cc28f8410ce8
Florian Robert <florian.robert@ekino.com>
2024-04-16 14:28:36 +0200

    add scroll to top on register button

--- src/app/core/theme/home/anonymous/anonymous.component.ts
+++ src/app/core/theme/home/anonymous/anonymous.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
+import { ViewportScroller } from '@angular/common'
 
 @Component({
   selector: 'app-anonymous',
   templateUrl: './anonymous.component.html',
 })
 export class AnonymousComponent {
-  constructor(public router: Router) {}
+  constructor(public router: Router, private viewportScroller: ViewportScroller) {}
+
+  scrollToTop(): void {
+    this.viewportScroller.scrollToPosition([0, 0])
+  }
 }
