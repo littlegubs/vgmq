aca20cd137644aafe57b514a2fd0c152ae01c7ad
Florian Robert <florian.robert@ekino.com>
2024-06-10 17:57:34 +0200

    unsubscribe observable

--- src/app/modules/admin/modules/users/components/users-admin/users.component.ts
+++ src/app/modules/admin/modules/users/components/users-admin/users.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { UsersHttpService } from '../../../../../../core/http/admin/users-http.service'
 import { UserFromAdmin } from '../../../../../../shared/models/user'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
 })
-export class UsersAdminComponent implements OnInit {
+export class UsersAdminComponent implements OnInit, OnDestroy {
+  usersObservable: Subscription
   users: UserFromAdmin[] | undefined
   constructor(private http: UsersHttpService) {}
 
   ngOnInit(): void {
-    this.http.getStats().subscribe((response) => {
+    this.usersObservable = this.http.getAllUsers().subscribe((response) => {
       this.users = response
     })
+  }
+
+  ngOnDestroy(): void {
+    this.usersObservable.unsubscribe()
   }
 }
