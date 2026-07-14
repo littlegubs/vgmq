0cacb2b129cbbfc1ad1231f4adc1ea523024d8aa
Alexis Leroy <alexis-60@hotmail.fr>
2026-07-14 16:32:24 +0200

    chore: optimized admin user page

--- src/app/modules/admin/modules/users/components/users-table/users-table.component.ts
+++ src/app/modules/admin/modules/users/components/users-table/users-table.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core'
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core'
 import { UserFromAdmin } from '../../../../../../shared/models/user'
 import { MatTableDataSource } from '@angular/material/table'
 import { MatPaginator } from '@angular/material/paginator'
@@ -6,7 +6,8 @@ import { MatSort } from '@angular/material/sort'
 import { MatDialog } from '@angular/material/dialog'
 import { BanDialogComponent } from './ban-dialog/ban-dialog.component'
 import { UsersHttpService } from '../../../../../../core/http/admin/users-http.service'
-import { Subscription } from 'rxjs'
+import { Subscription, merge, Subject } from 'rxjs'
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators'
 
 @Component({
   selector: 'app-users-table',
@@ -16,31 +17,59 @@ import { Subscription } from 'rxjs'
 export class UsersTableComponent implements AfterViewInit, OnInit, OnDestroy {
   dialogSubscription: Subscription | undefined
   getAllUsersSubscription: Subscription | undefined
-  @Input() users: UserFromAdmin[]
+  searchSubscription: Subscription | undefined
+
   displayedColumns = ['username', 'enabled', 'createdAt', 'banReason', 'bannedBy', 'actions']
-  dataSource: MatTableDataSource<UserFromAdmin>
+  dataSource = new MatTableDataSource<UserFromAdmin>([])
+  totalUsers = 0
+
+  searchSubject = new Subject<string>()
+  currentSearch = ''
 
   constructor(public dialog: MatDialog, private http: UsersHttpService) {}
 
   @ViewChild(MatPaginator) paginator: MatPaginator
   @ViewChild(MatSort) sort: MatSort
 
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator
-    this.dataSource.sort = this.sort
+  ngOnInit(): void {
+    this.searchSubscription = this.searchSubject
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((searchValue) => {
+        this.currentSearch = searchValue
+        this.paginator.pageIndex = 0
+        this.loadUsers()
+      })
   }
 
-  ngOnInit(): void {
-    this.dataSource = new MatTableDataSource(this.users)
+  ngAfterViewInit(): void {
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0))
+
+    merge(this.sort.sortChange, this.paginator.page).subscribe(() => {
+      this.loadUsers()
+    })
+
+    this.loadUsers()
+  }
+
+  loadUsers(): void {
+    this.getAllUsersSubscription?.unsubscribe()
+    this.getAllUsersSubscription = this.http
+      .getAllUsers(
+        this.paginator.pageIndex,
+        this.paginator.pageSize || 25,
+        this.currentSearch,
+        this.sort.active,
+        this.sort.direction ? this.sort.direction.toUpperCase() : 'DESC'
+      )
+      .subscribe((response) => {
+        this.dataSource.data = response.items
+        this.totalUsers = response.total
+      })
   }
 
   applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value
-    this.dataSource.filter = filterValue.trim().toLowerCase()
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage()
-    }
+    this.searchSubject.next(filterValue.trim().toLowerCase())
   }
 
   openBanDialog(user: UserFromAdmin): void {
@@ -49,9 +78,7 @@ export class UsersTableComponent implements AfterViewInit, OnInit, OnDestroy {
     })
     this.dialogSubscription = dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        this.getAllUsersSubscription = this.http.getAllUsers().subscribe((response) => {
-          this.dataSource = new MatTableDataSource(response)
-        })
+        this.loadUsers()
       }
     })
   }
@@ -60,5 +87,6 @@ export class UsersTableComponent implements AfterViewInit, OnInit, OnDestroy {
     this.dataSource.disconnect()
     this.dialogSubscription?.unsubscribe()
     this.getAllUsersSubscription?.unsubscribe()
+    this.searchSubscription?.unsubscribe()
   }
 }
